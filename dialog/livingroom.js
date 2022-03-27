6ce9bbf7f0a0c8157a98e3d66ca81815a4c15a3b
falseaxiom <mlegaspi@princeton.edu>
2022-03-26 23:55:00 -0400

    slightly less dummy dialog

---
+++ dialog/livingroom.js
@@ -0,0 +1,64 @@
+// ANGELA'S DIALOG
+angela1a = [
+    "angela",
+    "angela",
+    "angela",
+    "angela"
+]
+angela1b = [
+    "oh my gosh, hi drew!",
+    "i totally thought you weren't coming.",
+    "you let me know if you need anything, alright? <3",
+    "(and loosen up a little! it's a party, not a funeral!)",
+]
+
+// ELLIE'S DIALOG
+ellie1a = [
+    "ellie",
+    "ellie",
+    "ellie",
+    "ellie"
+]
+ellie1b = [
+    "oh, hey drew. good to see you.",
+    "...",
+    "... ... ...",
+    "um, yeah. that's all."
+]
+
+// JAKE'S DIALOG
+jake1a = [
+    "jake",
+    "jake",
+    "jake",
+    "jake"
+]
+jake1b = [
+    "well, lookie here--andy reyes, in the flesh!",
+    "see, told you guys he was coming.",
+    "go ahead, enjoy yourself! mi casa es su casa.",
+    "you should take off that jacket, though. you're gonna roast."
+]
+
+// BRAD'S DIALOG
+brad1a = [
+    "brad",
+    "brad",
+    "brad",
+    "brad"
+]
+brad1b = [
+    "yooo andy! what up, my man?",
+    "you want a beer? claw?",
+    "oh, shit, i forgot--you don't drink, haha.",
+    "well, lisa's got that green in the backyard if you're lookin for a puff, yeah?"
+]
+
+//SUZY'S DIALOG
+suzy1a = [
+    "suzy"
+]
+
+suzy1b = [
+    "this party is boring. when can we go home?"
+]
