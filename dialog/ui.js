1da2cafa6c6f3b9be04e26036fe654e86894e63e
falseaxiom <mlegaspi@princeton.edu>
2022-04-21 14:27:07 -0400

    separate ui js file

---
+++ dialog/ui.js
@@ -0,0 +1,21 @@
+// ui
+let map1a = [
+    "[ you've been to jake's house a couple times for the errant post-practice-turned-hangout. ]", 
+    "[ and for the even more sporadic house-party-you-feel-somewhat-obligated-to-attend. ]",
+    "[ you can't say you know its layout very well. ]"
+]
+let phone1a = [
+    "[ your CELL PHONE. a favored object of study when things get awkward. ]",
+    "CHOICE_CALL MOM^0_DROP ITEM^1_DO NOTHING^4",
+    "[ it's 2022. when's the last time anyone called anyone? ]^4",
+    "[ like hell you will!!!!!!!! ]",
+    "[ this is the first time you've gotten a PHONE brand-new, instead of one of your MAMA's old MOTOROLLO BLADRs. ]",
+    "[ you'd rather DIE than see it cracked. ]"
+]
+let yearbook2a = [
+    "[ west brunswick high school's 2021-2022 YEARBOOK. ]",
+    "CHOICE_SEE SIGNATURES^0_DROP ITEM^1_DO NOTHING^3",
+    "[ you're not wearing your contacts, so you can't read anything. ]^3",
+    "[ you don't really need a book full of things you'd rather forget. even if it's free. ]",
+    "DROP",
+]
