f128ea5a67a3718c09d9bc933277283876c4821f
falseaxiom <mlegaspi@princeton.edu>
2022-04-21 14:47:48 -0400

    Update ui.js

--- dialog/ui.js
+++ dialog/ui.js
@@ -15,7 +15,39 @@ let phone1a = [
 let yearbook2a = [
     "[ west brunswick high school's 2021-2022 YEARBOOK. ]",
     "CHOICE_SEE SIGNATURES^0_DROP ITEM^1_DO NOTHING^3",
-    "[ you're not wearing your contacts, so you can't read anything. ]^3",
+    "SIGS^3",
     "[ you don't really need a book full of things you'd rather forget. even if it's free. ]",
     "DROP",
+]
+let bracelet1a = [
+    "[ a BRACELET that you need to return to ANVI. ]",
+    "CHOICE_INSPECT ITEM^0_DROP ITEM^3_DO NOTHING^5",
+    "[ something about this BRACELET feels very familiar... ]",
+    "[ you can't quite put your finger on it, though. ]",
+    "[ maybe SUZY owns a similar one? ]^3",
+    "[ you try to slip the BRACELET into a nearby drawer... but you stop yourself. ]",
+    "[ it just doesn't feel right. ]"
+]
+let scrunchie1a = [
+    "[ a SCRUNCHIE that you need to return to RYAN. ]",
+    "CHOICE_INSPECT ITEM^0_DROP ITEM^2_DO NOTHING^4",
+    "[ you didn't think RYAN's hair was long enough to put in a SCRUNCHIE. ]",
+    "[ but then again, you haven't seen him in a while. ]^3",
+    "[ you try to slip the SCRUNCHIE into a nearby drawer... but you stop yourself. ]",
+    "[ it just doesn't feel right. ]"
+]
+let lighter1a = [
+    "[ a LIGHTER that you need to return to LISA. ]",
+    "CHOICE_INSPECT ITEM^0_DROP ITEM^2_DO NOTHING^4",
+    "[ you flick the LIGHTER a couple times. nothing happens. ]",
+    "[ BRAD must've burnt it out... what's the point of returning it, then? ]^3",
+    "[ you try to slip the SCRUNCHIE into a nearby drawer... but you stop yourself. ]",
+    "[ it just doesn't feel right. ]"
+]
+let trash1a = [
+    "[ a bunch of assorted TRASH you've been picking up. ]",
+    "CHOICE_COUNT TRASH^0_DROP ITEM^1_DO NOTHING^2",
+    "[ you have collected ### pieces of TRASH so far. ]^2",
+    "[ you try to find a nearby TRASH CAN to dump it in, but there seem to be none around. ]",
+    "[ (looks like the artist forgot to include them...) ]"
 ]
