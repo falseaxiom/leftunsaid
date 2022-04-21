44570f8d3375b7c5e36be8f051d6a6ab4e40bdb0
falseaxiom <mlegaspi@princeton.edu>
2022-04-21 14:22:39 -0400

    front yard done, living room start

---
+++ dialog/frontyard.js
@@ -0,0 +1,182 @@
+// automatic dialog
+let suzy1a = [
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:"
+]
+let suzy1b = [
+    "finally, we're here!!!",
+    "you were driving like an old lady the whole way.",
+    "i bet i could've gotten us here faster.",
+    "CHOICE_you don't even have your license yet.^0_big talk for someone who can't reach the brake pedal.^0",
+    "i still know more road rules than you, though.",
+    "ate KITKAT told me if a stop sign has a white border, it's optional.<br/><br/><br/><br/><em style='color:var(--d)'>(A/N: \"ate\" is a filipino term for big sister, but is usually used to refer to any older girl.)</em>",
+    "but you stopped at every single one of them!",
+    "CHOICE_KITKAT failed her driver's test twice.^0_did she also tell you if all three traffic lights turn on at once, you get $10?^1",
+    "well, i still trust her more than you.^3",
+    "...",
+    "well, that's just not true.",
+    "but whatever.",
+    "i thought having an older brother would mean i got to go to more parties. and a varsity athlete, no less!",
+    "but nooo, all you do is stay at home and study. and crochet.",
+    "***like an old lady.***",
+    "and when you do go to parties, you don't bring me!",
+    "now my first ever house party is your stupid swim team grad party. i'm not gonna know a single person there.",
+    "CHOICE_like i said, you didn't have to come.^0",
+    "oh, so i was supposed to pass up an opportunity to blackmail you?",
+    "it's not every day we run into JAKE GIORDANO while buying craft yarn.",
+    "\"uhh, i'm holding this for my sister!!\"",
+    "\"my name is DREW REYES and i can't admit to having traditionally feminine hobbies in front of my guy friends!!\"",
+    "i could've *destroyed* your social life right then and there.",
+    "but i kept that secret for the rest of may, like the best baby sister ever, asking for nothing in return.",
+    "CHOICE_except for a ride to JAKE's party.^0",
+    "exactly. so let's get moving!!!"
+]
+let jake1a = [
+    "JAKE:",
+    "SUZY:",
+    "",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "",
+    "JAKE:",
+    "JAKE:",
+    "SUZY:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "",
+    "JAKE:",
+    "",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:"
+]
+let jake1b = [
+    "well well well, if it isn't ANDY REYES in the flesh!",
+    "(andy??)",
+    "[ DREW is not short for ANDREW. it's not short for anything. JAKE knows this. ]",
+    "so glad you could fit us into your busy schedule.",
+    "y'know, everyone said you weren't gonna show, but i was like, \"hey, we're graduating! you think he'd pass up one last rager?\"",
+    "and they were like, \"yeah, probably.\"",
+    "but here you are, proving everyone wrong.",
+    "AND you brought a girl! full of surprises, this guy.",
+    "don't think we've met before. i'm JAKE.",
+    "CHOICE_SUZY. my little sister.^0",
+    "right! the knitting girl, with the yarn.",
+    "sorry about that. ANDY doesn't talk about you much.",
+    "i prefer it that way, actually. less association between us.",
+    "haha, burn! i like you already.",
+    "well, come on in, make yourself at home. there's pizza and booze.",
+    "\"mi casa es tu casa\" and all that.",
+    "[ SUZY walks in first, and you follow suit - but JAKE stops you. ]",
+    "(your sister. is she single?)",
+    "CHOICE_did you really just ask me that?^0_she's a freshman, creep.^1_what about ANGELA?^2",
+    "(whaaat? it's just a joke!)^3",
+    "(kidding! jeez. lighten up, reyes.^2",
+    "(ah, we're on a break right now. you know how ANGIE is.)",
+    "anyway. let's go to the living room first - everyone's gotta know you're alive."
+]
+
+// characters
+let suzy2a = [
+    "",
+    "",
+    "SUZY:",
+    "",
+    ""
+]
+let suzy2b = [
+    "[ SUZY REYES, your little sister. ]",
+    "CHOICE_TALK TO SUZY^0_yyASK TO SIGN YEARBOOK^1_DO NOTHING^3",
+    "what are we still doing standing outside? there's a party waiting for us!!^3",
+    "[ she literally lives in the same house as you. what would she even write? \"have a good summer\"? ]",
+    "[ she'd probably make your summer worse just for asking her. ]"
+]
+
+// interactive items
+let tesla1a = [
+    "[ JAKE's TESLO MODEL W. he always parks it outside his garage, like an asshole. ]",
+    "CHOICE_KEY JAKE'S CAR^0_BEGRUDGINGLY ADMIRE JAKE'S CAR^4_DO NOTHING^6",
+    "[ the glossy, black finish beckons. your hand twitches at your KEYS. ]",
+    "[ one little nick couldn't hurt... right...? ]",
+    "[ ... ]",
+    "[ nah. you shouldn't. ]^3",
+    "[ oh, if only TESLOs weren't exclusively owned by rich jerks. ]",
+    "[ although you guess it doesn't help that they were invented by a rich jerk. ]"
+]
+let toyota1a = [
+    "[ your beat-up 2008 TOYOTO CAMRY. she's not the fastest, nor the most reliable, but she's all yours. ]",
+    "CHOICE_DRIVE BACK HOME^0_KEY YOUR CAR^2_DO NOTHING^3",
+    "[ honestly, not a bad idea. ]",
+    "[ but with gas prices the way they are... yeah. nevermind. ]^2",
+    "[ why would you do that. ]"
+]
+let bush1a = [
+    "[ JAKE lives in the nicer part of town, which you can tell from this non-native, expensive-looking SHRUBBERY. ]",
+    "[ it probably sucks up enough water to fill a swimming pool. ]",
+    "CHOICE_PISS IN THEM^0_DO NOTHING^2",
+    "[ what? no. you're not a degenerate. ]",
+    "[ and NICK JEONG will probably do that later, anyway. ]"
+]
+let yearbook1a = [
+    "[ someone left their YEARBOOK on the front lawn. don't these things cost $200? ]",
+    "CHOICE_PICK UP^0_LEAVE IT^2",
+    "[ guess it's yours now. ]",
+    "PICKUP"
+]
+let frontdoor1a = [
+    "[ the FRONT DOOR to JAKE's house. ]",
+    "CHOICE_KNOCK ON FRONT DOOR^0_DO NOTHING^1",
+    "KNOCKKNOCK"
+]
+
+// ui
+let map1a = [
+    "[ you've been to jake's house a couple times for the errant post-practice-turned-hangout. ]", 
+    "[ and for the even more sporadic house-party-you-feel-somewhat-obligated-to-attend. ]",
+    "[ you can't say you know its layout very well. ]"
+]
+let phone1a = [
+    "[ your CELL PHONE. a favored object of study when things get awkward. ]",
+    "CHOICE_CALL MOM^0_DROP ITEM^1_DO NOTHING^4",
+    "[ it's 2022. when's the last time anyone called anyone? ]^4",
+    "[ like hell you will!!!!!!!! ]",
+    "[ this is the first time you've gotten a PHONE brand-new, instead of one of your MAMA's old MOTOROLLO BLADRs. ]",
+    "[ you'd rather DIE than see it cracked. ]"
+]
+let yearbook2a = [
+    "[ west brunswick high school's 2021-2022 YEARBOOK. ]",
+    "CHOICE_SEE SIGNATURES^0_DROP ITEM^1_DO NOTHING^3",
+    "[ you're not wearing your contacts, so you can't read anything. ]^3",
+    "[ you don't really need a book full of things you'd rather forget. even if it's free. ]",
+    "DROP",
+]
--- dialog/livingroom.js
+++ dialog/livingroom.js
@@ -1,117 +1,232 @@
-// ANGELA'S DIALOG
-angela1a = [
-    "angela",
-    "angela",
-    "angela",
-    "angela"
+// automatic dialog
+autoa = [
+    "ANGELA:",
+    "ELLIE:",
+    "BRAD:",
+    "JAKE:",
+    "JAKE:",
+    "ANGELA:",
+    "SUZY:",
+    "",
+    "ELLIE:",
+    "SUZY:",
+    "ELLIE:",
+    "BRAD:",
+    "",
+    "ELLIE:",
+    "BRAD:",
+    "SUZY:",
+    "BRAD:",
+    "BRAD:",
+    "BRAD:",
+    "ANGELA:",
+    "BRAD:",
+    "BRAD:"
 ]
-angela1b = [
-    "oh my gosh, hi drew!",
-    "i totally thought you weren't coming.",
-    "you let me know if you need anything, alright? <3",
-    "(and loosen up a little! it's a party, not a funeral!)",
+autob = [
+    "oh my gosh, hi DREW!!",
+    "DREW REYES, as i live and breathe.",
+    "yooo andy, what up! feels like ages since you last partied with us.",
+    "see? told you guys he was coming.",
+    "and he brought his little sister, too.",
+    "aww, she looks just like you :)",
+    "that's the worst thing anyone's ever said about me.",
+    "[ EVERYONE laughs. ]",
+    "what year are you?",
+    "freshman.",
+    "you thinking of doing swim next year?",
+    "dub's losing their best flyer, after all!",
+    "[ BRAD pats your back a little too hard. you try to suppress a cough. ]",
+    "she wouldn't be on the men's team, idiot.",
+    "she could do mixed medley, though!",
+    "i'm not really a sports person.",
+    "right on. you seem like you're too cool for swim, anyway.",
+    "well, hey - enjoy the party!",
+    "and if you ever need a little something, you can always ask your uncle BRAD.",
+    "please don't give weed to freshmen, BRAD.",
+    "i think she's old enough to make her own decisions.",
+    "you, too, ANDY - you've gotta try it at least once before we go!"
 ]
 
-// ELLIE'S DIALOG
-ellie1a = [
-    "ellie",
-    "ellie",
-    "ellie",
-    "ellie"
+
+let suzy1a = [
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "SUZY:",
+    "",
+    "SUZY:"
 ]
-ellie1b = [
-    "oh, hey drew. good to see you.",
+let suzy1b = [
+    "finally, we're here!!!",
+    "you were driving like an old lady the whole way.",
+    "i bet i could've gotten us here faster.",
+    "CHOICE_you don't even have your license yet.^0_big talk for someone who can't reach the brake pedal.^0",
+    "i still know more road rules than you, though.",
+    "ate KITKAT told me if a stop sign has a white border, it's optional.<br/><br/><br/><br/><em style='color:var(--d)'>(A/N: \"ate\" is a filipino term for big sister, but is usually used to refer to any older girl.)</em>",
+    "but you stopped at every single one of them!",
+    "CHOICE_KITKAT failed her driver's test twice.^0_did she also tell you if all three traffic lights turn on at once, you get $10?^1",
+    "well, i still trust her more than you.^3",
     "...",
-    "... ... ...",
-    "um, yeah. that's all."
+    "well, that's just not true.",
+    "but whatever.",
+    "i thought having an older brother would mean i got to go to more parties. and a varsity athlete, no less!",
+    "but nooo, all you do is stay at home and study. and crochet.",
+    "***like an old lady.***",
+    "and when you do go to parties, you don't bring me!",
+    "now my first ever house party is your stupid swim team grad party. i'm not gonna know a single person there.",
+    "CHOICE_like i said, you didn't have to come.^0",
+    "oh, so i was supposed to pass up an opportunity to blackmail you?",
+    "it's not every day we run into JAKE GIORDANO while buying craft yarn.",
+    "\"uhh, i'm holding this for my sister!!\"",
+    "\"my name is DREW REYES and i can't admit to having traditionally feminine hobbies in front of my guy friends!!\"",
+    "i could've *destroyed* your social life right then and there.",
+    "but i kept that secret for the rest of may, like the best baby sister ever, asking for nothing in return.",
+    "CHOICE_except for a ride to JAKE's party.^0",
+    "exactly. so let's get moving!!!"
 ]
-
-// JAKE'S DIALOG
-jake1a = [
-    "jake",
-    "jake",
-    "jake",
-    "jake"
-]
-jake1b = [
-    "well, lookie here--andy reyes, in the flesh!",
-    "see, told you guys he was coming.",
-    "go ahead, enjoy yourself! mi casa es su casa.",
-    "you should take off that jacket, though. you're gonna roast."
-]
-
-// BRAD'S DIALOG
-brad1a = [
-    "brad",
-    "brad",
-    "brad",
-    "brad"
-]
-brad1b = [
-    "yooo andy! what up, my man?",
-    "can i hit you up with something? beer? claw?",
-    "oh, shit, i forgot--you don't drink, haha.",
-    "well, lisa's got that green in the backyard if you're lookin' for a smoke, yeah?"
-]
-
-//SUZY'S DIALOG
-suzy1a = [
-    "suzy",
+let jake1a = [
+    "JAKE:",
+    "SUZY:",
     "",
-    "suzy",
-    "suzy",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
     "",
-    "suzy"
+    "JAKE:",
+    "JAKE:",
+    "SUZY:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "",
+    "JAKE:",
+    "",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:",
+    "JAKE:"
+]
+let jake1b = [
+    "well well well, if it isn't ANDY REYES in the flesh!",
+    "(andy??)",
+    "[ DREW is not short for ANDREW. it's not short for anything. JAKE knows this. ]",
+    "so glad you could fit us into your busy schedule.",
+    "y'know, everyone said you weren't gonna show, but i was like, \"hey, we're graduating! you think he'd pass up one last rager?\"",
+    "and they were like, \"yeah, probably.\"",
+    "but here you are, proving everyone wrong.",
+    "AND you brought a girl! full of surprises, this guy.",
+    "don't think we've met before. i'm JAKE.",
+    "CHOICE_SUZY. my little sister.^0",
+    "right! the knitting girl, with the yarn.",
+    "sorry about that. ANDY doesn't talk about you much.",
+    "i prefer it that way, actually. less association between us.",
+    "haha, burn! i like you already.",
+    "well, come on in, make yourself at home. there's pizza and booze.",
+    "\"mi casa es tu casa\" and all that.",
+    "[ SUZY walks in first, and you follow suit - but JAKE stops you. ]",
+    "(your sister. is she single?)",
+    "CHOICE_did you really just ask me that?^0_she's a freshman, creep.^1_what about ANGELA?^2",
+    "(whaaat? it's just a joke!)^3",
+    "(kidding! jeez. lighten up, reyes.^2",
+    "(ah, we're on a break right now. you know how ANGIE is.)",
+    "anyway. let's go to the living room first - everyone's gotta know you're alive."
 ]
 
-suzy1b = [
-    "this party is boring. when can we go home?",
-    "$$$_we'll leave soon.^0-1_maybe you shouldn't have begged me to come along.^1-1",
-    "thank god.",
-    "well, i didn't know it was gonna be just for seniors!"
+// characters
+let suzy2a = [
+    "",
+    "",
+    "SUZY:",
+    "",
+    ""
+]
+let suzy2b = [
+    "[ SUZY REYES, your little sister. ]",
+    "CHOICE_TALK TO SUZY^0_yyASK TO SIGN YEARBOOK^1_DO NOTHING^3",
+    "what are we still doing standing outside? there's a party waiting for us!!^3",
+    "[ she literally lives in the same house as you. what would she even write? \"have a good summer\"? ]",
+    "[ she'd probably make your summer worse just for asking her. ]"
 ]
 
-// INTERACTIVE ITEMS
-couch = [
-    "a nice green COUCH. nothing wrong with that."
+// interactive items
+let tesla1a = [
+    "[ JAKE's TESLO MODEL W. he always parks it outside his garage, like an asshole. ]",
+    "CHOICE_KEY JAKE'S CAR^0_BEGRUDGINGLY ADMIRE JAKE'S CAR^4_DO NOTHING^6",
+    "[ the glossy, black finish beckons. your hand twitches at your KEYS. ]",
+    "[ one little nick couldn't hurt... right...? ]",
+    "[ ... ]",
+    "[ nah. you shouldn't. ]^3",
+    "[ oh, if only TESLOs weren't exclusively owned by rich jerks. ]",
+    "[ although you guess it doesn't help that they were invented by a rich jerk. ]"
 ]
-lamp = [
-    "a LAMP. it doesn't seem to turn on."
+let toyota1a = [
+    "[ your beat-up 2008 TOYOTO CAMRY. she's not the fastest, nor the most reliable, but she's all yours. ]",
+    "CHOICE_DRIVE BACK HOME^0_KEY YOUR CAR^2_DO NOTHING^3",
+    "[ honestly, not a bad idea. ]",
+    "[ but with gas prices the way they are... yeah. nevermind. ]^2",
+    "[ why would you do that. ]"
 ]
-joint = [
-    "an unsmoked JOINT. looks like BRAD dropped this.",
-    "well, it's yours now.",
-    "***"
+let bush1a = [
+    "[ JAKE lives in the nicer part of town, which you can tell from this non-native, expensive-looking SHRUBBERY. ]",
+    "[ it probably sucks up enough water to fill a swimming pool. ]",
+    "CHOICE_PISS IN THEM^0_DO NOTHING^2",
+    "[ what? no. you're not a degenerate. ]",
+    "[ and NICK JEONG will probably do that later, anyway. ]"
 ]
-rplay = [
-    "a RECORD PLAYER. right now, it's playing mitski.",
-    "a weird vibe for a high school jock party."
+let yearbook1a = [
+    "[ someone left their YEARBOOK on the front lawn. don't these things cost $200? ]",
+    "CHOICE_PICK UP^0_LEAVE IT^2",
+    "[ guess it's yours now. ]",
+    "PICKUP"
 ]
-rbox = [
-    "a BOX full of VINYL RECORDS.",
-    "...it's all mitski..."
-]
-ps5 = [
-    "IT'S ME, BOY. I'M THE PS5.",
-    "SPEAKING TO YOU FROM INSIDE YOUR BRAIN.",
-    "LISTEN TO ME, BOY.",
-    "LEAVE THE GIRL. YOU DON'T NEED HER."
-]
-tv = [
-    "the TV is playing monday night football.",
-    "who's even watching this? everyone here is on the swim team."
+let frontdoor1a = [
+    "[ the FRONT DOOR to JAKE's house. ]",
+    "CHOICE_KNOCK ON FRONT DOOR^0_DO NOTHING^1",
+    "KNOCKKNOCK"
 ]
 
-// INVENTORY ITEMS
-iphone = [
-    "your CELL PHONE. a favored object of study when things get awkward."
+// ui
+let map1a = [
+    "[ you've been to jake's house a couple times for the errant post-practice-turned-hangout. ]", 
+    "[ and for the even more sporadic house-party-you-feel-somewhat-obligated-to-attend. ]",
+    "[ you can't say you know its layout very well. ]"
 ]
-icup = [
-    "a CUP of... some kind of liquid. you're not sure what it is exactly, but it's probably alcoholic."
+let phone1a = [
+    "[ your CELL PHONE. a favored object of study when things get awkward. ]",
+    "CHOICE_CALL MOM^0_DROP ITEM^1_DO NOTHING^4",
+    "[ it's 2022. when's the last time anyone called anyone? ]^4",
+    "[ like hell you will!!!!!!!! ]",
+    "[ this is the first time you've gotten a PHONE brand-new, instead of one of your MAMA's old MOTOROLLO BLADRs. ]",
+    "[ you'd rather DIE than see it cracked. ]"
 ]
-ibook = [
-    "a high school YEARBOOK. you don't really know why you're still carrying this around."
-]
-ijoint = [
-    "BRAD's fallen JOINT. it's expertly rolled--almost too good to be brad's handiwork."
+let yearbook2a = [
+    "[ west brunswick high school's 2021-2022 YEARBOOK. ]",
+    "CHOICE_SEE SIGNATURES^0_DROP ITEM^1_DO NOTHING^3",
+    "[ you're not wearing your contacts, so you can't read anything. ]^3",
+    "[ you don't really need a book full of things you'd rather forget. even if it's free. ]",
+    "DROP",
 ]
