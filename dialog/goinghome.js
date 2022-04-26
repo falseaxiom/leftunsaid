0fc0946fa38f5c5000d4be771b478853419836c0
falseaxiom <mlegaspi@princeton.edu>
2022-04-26 16:02:14 -0400

    GAME IS NOW PLAYABLE :D

---
+++ dialog/goinghome.js
@@ -0,0 +1,41 @@
+// automatic dialog
+let anviEnda = [
+    "",
+    "ANVI:",
+    "ANVI:",
+    "ANVI:",
+    "ANVI:",
+    "ANVI:"
+]
+let anviEndb = [
+    "[ ANVI helps you walk SUZY outside. she gently places SUZY in the backseat of your car. ]",
+    "...",
+    "haven't seen SUZY in a while. it was nice hanging out with her again.",
+    "even though she was throwing up for most of it. haha.",
+    "...",
+    "anyway bye"
+]
+let ryanEnda = [
+    "",
+    "RYAN:"
+]
+let ryanEndb = [
+    "[ RYAN helps you walk SUZY outside. he gently places SUZY in the backseat of your car. ]",
+    "ya bye"
+]
+let lisaEnda = [
+    "",
+    "LISA:"
+]
+let lisaEndb = [
+    "[ RYAN helps you walk SUZY outside. he gently places SUZY in the backseat of your car. ]",
+    "bye lol"
+]
+let suzyEnda = [
+    "",
+    "SUZY:"
+]
+let suzyEndb = [
+    "[ you walk SUZY outside and guide her in the backseat of your car. ]",
+    "i'm gonna throw up houuuuuughghohgguhogu"
+]
